6151e17d76dc2cc885367a192f37c8517daf2cd7
Murli Kumar <aimgodhere@gmail.com>
2026-02-27 11:49:36 +0530

    clean up the code by using module.exports way(1)

---
+++ app.js
@@ -0,0 +1,7 @@
+const http = require("http");
+const routes = require("./routes");
+const server = http.createServer(routes);
+
+server.listen(3000, () => {
+  console.log("Server is running on port:3000");
+});
---
+++ routes.js
@@ -0,0 +1,59 @@
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+  //url,method
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.end(
+      `<form action="/message" method="POST">
+        <label>Name:</label>
+        <input type="text" name="name"></input>
+        <button type="submit">Add</button>
+      </form>
+      `,
+    );
+  } else {
+    if (url === "/message") {
+      // console.log("message url hit");
+      // res.end("<h1>Hello from message</h1>")
+
+      let body = [];
+
+      req.on("data", (chunks) => {
+        body.push(chunks);
+      });
+
+      req.on("end", () => {
+        let buffer = Buffer.concat(body);
+        console.log(buffer);
+
+        let formData = buffer.toString();
+        console.log(formData);
+
+        const formValues = formData.split("=")[1];
+
+        fs.writeFile("formValues2.txt", formValues, (err) => {
+          res.statusCode = 302;
+          res.setHeader("Location", "/");
+          res.end();
+        });
+      });
+    } else {
+      if (url === "/read") {
+        //read from file
+
+        fs.readFile("formValues2.txt", (err, data) => {
+          // console.log(data.toString());
+
+          res.end(`
+            <h1>${data.toString()}</h1>`);
+        });
+      }
+    }
+  }
+};
+
+module.exports = requestHandler;
